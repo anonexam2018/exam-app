f70b55d7cbfed9a16a282a8609c5a96831bb42bd
Ola Kveli <anonexam@yahoo.com>
2019-02-24 23:21:49 +0100

    docker attempt

---
+++ src/test/com/example/herokupipeexample/SimpleTest.java
@@ -0,0 +1,13 @@
+package com.example.herokupipeexample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SimpleTest {
+
+    @Test
+	public void simpleTester(){
+		assertTrue(true);
+	}
+}
